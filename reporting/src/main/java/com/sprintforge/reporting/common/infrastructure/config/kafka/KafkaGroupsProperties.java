1eea0c6e741ee60ab1d5d8b3596f3c6fefd00ff0
Mariano Camposeco <mariano1941@outlook.es>
2025-12-26 19:16:01 -0600

    feat: add kafka configuration

---
+++ reporting/src/main/java/com/sprintforge/reporting/common/infrastructure/config/kafka/KafkaGroupsProperties.java
@@ -0,0 +1,10 @@
+package com.sprintforge.reporting.common.infrastructure.config.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KafkaGroupsProperties {
+    private String defaultGroup;
+}
---
+++ reporting/src/main/java/com/sprintforge/reporting/common/infrastructure/config/kafka/KafkaTopicsProperties.java
@@ -0,0 +1,9 @@
+package com.sprintforge.reporting.common.infrastructure.config.kafka;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KafkaTopicsProperties {
+}
